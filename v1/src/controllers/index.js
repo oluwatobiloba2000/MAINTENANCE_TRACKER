44a66804b11bde6a08e6910e90e9d044d51642de
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-08-06 21:03:39 +0100

    added request controller for getting request by unique ID

--- v1/src/controllers/index.js
+++ v1/src/controllers/index.js
@@ -1,10 +1,31 @@
 import uuid from "uuid/v1";
-import {requests, newDate} from "../data/data";
+import {
+    requests,
+    newDate
+} from "../data/data";
 let requestParsed = JSON.parse(requests);
 class RequestControllers {
+    
     // getting all requests
     static allRequests(req, res) {
-       return res.status(200).json(requestParsed);
+        return res.status(200).json(requestParsed);
+    }
+
+    //GET /requests/:<request id>
+    static getRequestById(req, res) {
+        const id = req.params.id;
+        const request = requestParsed.find(each =>
+            each.id == id
+        )
+        if (!request) {
+            res.status(404).json({
+                message: `request with id: ${id} not found`
+            });
+        }
+        res.status(200).json({
+            request
+        });
+
     }
 }
 
