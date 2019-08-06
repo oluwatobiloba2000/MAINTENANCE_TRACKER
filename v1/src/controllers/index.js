79afba8fb4a31ac38ee0a639f03d1a8f17b0f31c
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-08-06 21:01:25 +0100

    controller for getting all requests from data file

--- v1/src/controllers/index.js
+++ v1/src/controllers/index.js
@@ -1,2 +1,11 @@
 import uuid from "uuid/v1";
 import {requests, newDate} from "../data/data";
+let requestParsed = JSON.parse(requests);
+class RequestControllers {
+    // getting all requests
+    static allRequests(req, res) {
+       return res.status(200).json(requestParsed);
+    }
+}
+
+export default RequestControllers;
