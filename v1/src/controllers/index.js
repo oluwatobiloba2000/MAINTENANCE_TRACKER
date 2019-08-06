3002053925e42531293132105233e754dddf1adf
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-08-06 21:04:59 +0100

    added request controller for posting a request

--- v1/src/controllers/index.js
+++ v1/src/controllers/index.js
@@ -5,7 +5,7 @@ import {
 } from "../data/data";
 let requestParsed = JSON.parse(requests);
 class RequestControllers {
-    
+
     // getting all requests
     static allRequests(req, res) {
         return res.status(200).json(requestParsed);
@@ -26,6 +26,36 @@ class RequestControllers {
             request
         });
 
+    }
+
+    //sending a post request
+    static createRequest(req, res) {
+        const title = req.body.title;
+        const category = req.body.category;
+        const description = req.body.description;
+
+        if (!title || !category || !description) {
+            return res.status(400).json({
+                message: `request must have a title , post category and post description`
+            });
+        }
+
+        const id = uuid();
+        const request = {
+            id: id,
+            title: title,
+            category: category,
+            description: description,
+            time: newDate,
+            status: pending
+        }
+        // push the new data into the database
+        requestPased.push(request);
+        return res.json({
+            message: `request posted !`,
+
+            request
+        })
     }
 }
 
