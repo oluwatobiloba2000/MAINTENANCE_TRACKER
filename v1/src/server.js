ca11a2145b88a1f3f6dca17cba25d9ac81f873e5
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-09-04 18:02:30 +0100

    allow control origin

--- v1/src/server.js
+++ v1/src/server.js
@@ -29,17 +29,39 @@ app.use(bodyParser.json({
 }));
 
 
-app.use((req, res, next) => {
-     // allow all routes
-   res.setHeader('Access-Control-Allow-Origin', '*');
+// app.use((req, res, next) => {
+//      // allow all routes
+//    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // allow methods
+//     // allow methods
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+
+//     // allow request headers
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// })
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://tracky-maintenance-app.herokuapp.com');
+
+  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
 
-    // allow request headers
+  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
-})
+});
+
+
 // configure path to load html files
 const frontend = path.join(__dirname, '../../UI');
 
